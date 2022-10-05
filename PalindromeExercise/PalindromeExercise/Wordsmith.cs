171697e339f732664fd695f36a3ed0f984601f04
cmreavis97 <cmreavis97@gmail.com>
2022-10-05 10:46:47 -0400

    first commit

---
+++ PalindromeExercise/PalindromeExercise/Wordsmith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class Wordsmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            return word.SequenceEqual(word.Reverse());
+            //throw new NotImplementedException();
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("rotor", true)]
+        [InlineData("baseball", false)]
+        [InlineData("level", true)]
+        [InlineData("deter", false)]
+        [InlineData("antidisestablishmentarianism", false)]
+        public void PalindromeTest(string word1, bool isPalindromeExpected)
         {
+            //Arrange
+            var pal = new Wordsmith();
+            //Act
+            var isPalindromeActual = pal.IsAPalindrome(word1);
+            //Assert
+            Assert.Equal(isPalindromeExpected, isPalindromeActual);
 
         }
     }
